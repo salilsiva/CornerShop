002ee393cd95d30855a9f60f7fc506ebac5e6212
Salil <salil_siva@yahoo.com>
2026-06-15 18:04:33 +0100

    Removed the piece where the login data is showing on the bar

--- apps/web-store-angular/src/app/app.component.ts
+++ apps/web-store-angular/src/app/app.component.ts
@@ -12,8 +12,6 @@ import { CartService } from './services/cart/cart.service';
 })
 export class AppComponent implements OnInit {
   title = 'web-store-angular';
-  username : string | null = '';
-  displayName: string | null = '';
   cartCount = 1;
 
   constructor(public auth: AuthService, private cartService: CartService, private router: Router){}
@@ -22,9 +20,8 @@ export class AppComponent implements OnInit {
     this.cartService.cartCount$.subscribe(count => {
     this.cartCount = count;
   });
-  this.username = localStorage.getItem('username') || sessionStorage.getItem('username') || '';
-  this.displayName = this.username.split('@')[0];
   }
+  
   logout(){
     this.auth.logout();
     this.router.navigate(["/login"]);
